7710a9f4c251597ef38ab67308b73dbca49d10d8
RaghavendraPeetha <87773958+RaghavendraPeetha@users.noreply.github.com>
2024-05-24 13:50:18 +0530

    Update Multiple disease pred.py

--- Multiple disease pred.py
+++ Multiple disease pred.py
@@ -9,9 +9,9 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-diabetes_model=pickle.load(open('D:/ML_practice/Multiple disease prediction/diabetes_model.sav','rb'))
-heart_disease_model=pickle.load(open('D:/ML_practice/Multiple disease prediction/heart_disease_model.sav','rb'))
-parkinsons_model=pickle.load(open('D:/ML_practice/Multiple disease prediction/parkinsons_model.sav','rb'))
+diabetes_model=pickle.load(open('diabetes_model.sav','rb'))
+heart_disease_model=pickle.load(open('heart_disease_model.sav','rb'))
+parkinsons_model=pickle.load(open('parkinsons_model.sav','rb'))
 
 
 with st.sidebar:
